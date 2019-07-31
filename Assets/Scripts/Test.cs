c3896c9efcd90ca3132e0b337d1240379ea4e668
Guillaume <pastor.guill@gmail.com>
2019-07-31 17:47:54 +0200

    Add Test Scene.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(cube.transform.position.x, 5 + i * 5 , cube.transform.position.z);
+            cube.AddComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SummerJam2019
+
+## Unity and VSCode
+
+https://code.visualstudio.com/docs/other/unity
+https://dotnet.microsoft.com/download/thank-you/net471-developer-pack
