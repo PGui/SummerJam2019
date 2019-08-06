2776750b1375a02d5e0ae291a6b356486ee36e38
Bastien <babas-91@hotmail.fr>
2019-08-06 14:50:03 +0200

    New imputs to handle up to 8 players
    Update character selection scene to spawn players

--- Assets/ThirdParty/MultiplayerInput/Coop UI/Scripts/Managers/PlayerManager.cs
+++ Assets/ThirdParty/MultiplayerInput/Coop UI/Scripts/Managers/PlayerManager.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using XInputDotNetPure; // Required in C#
 using UnityEngine.SceneManagement;
+
 
 namespace LocalCoop {
     /// <summary>
@@ -10,167 +10,211 @@ namespace LocalCoop {
     /// </summary>
     public class PlayerManager : MonoBehaviour {
 
-        // PlayerIndex controllerID1;
-        // PlayerIndex controllerID2;
-        // PlayerIndex controllerID3;
-        // PlayerIndex controllerID4;
-        // GamePadState controller1state;
-        // GamePadState controller2state;
-        // GamePadState controller3state;
-        // GamePadState controller4state;
+        [System.Serializable]
+        public class PlayerData
+        {    
+            public int controllerID = 0;
+            public bool isActive = false; //Used to sapwn/unspawn characters
+            public bool isReady = false; //Used to set player as "ready" (when all are ready, Load Level scene)
+            private bool isDPadPressed = false; //Handle the release of DPad
+            public GameObject playerGameObject;
+            public void Spawn(GameObject PlayerPrefab)
+            {
+                //Spawn and store player prefab in GameObject
+                //Set the Player ID in PlayerController for input strings matching
+                GameObject player = Instantiate(PlayerPrefab, new Vector3 (0,0,0), Quaternion.identity) as GameObject; 
+                player.transform.parent = GameObject.Find("PlayerManager").transform;
+                player.GetComponent<FakePlayerController>().playerControllerID = controllerID;
+                playerGameObject = player;
+            }
+            public void Activate()
+            {
+                isActive = true;
+            }
+            public void Ready()
+            {
+                isReady = true;
+            }
+            public void Deactivate()
+            {
+                isActive = false;
+                isReady = false;
+            }
+            public void ToggleGameplayControls(bool isEnabled)
+            {
+                playerGameObject.GetComponent<FakePlayerController>().gameplayControlsEnabled = isEnabled;
+            }
+            public void ReleaseDPad()
+            {
+                isDPadPressed = false;
+            }
+            public void SelectNextCharacter()
+            {
+                if(!isDPadPressed)
+                {
+                    isDPadPressed = true;
+                    playerGameObject.GetComponent<FakeCharacterSelector>().OnDPadRightPressed();
+                }
+            }
+            public void SelectPreviousCharacter()
+            {
+                if(!isDPadPressed)
+                {
+                    isDPadPressed = true;
+                    playerGameObject.GetComponent<FakeCharacterSelector>().OnDPadLeftPressed();
+                }
+            }
+        }
+        
+        public static int K_NB_PLAYER_MAX = 8;
+        public static string K_HORIZONTAL = "Horizontal_P";
+        public static string K_HORIZONTAL_DPAD = "Horizontal_DPad_P";
+        public static string K_VERTICAL = "Vertical_P";
+        public static string K_JUMP = "Jump_P";
+        public static string K_DASH = "Dash_P";        
+        public GameObject CharacterSelectionUIPrefab;
+        public GameObject PlayerPrefab;
+        public List<PlayerData> playerListDyn;
 
-        public bool use_X_Input = true;
-        public int connectedControllers = 0;   //if this variable changes, we need to call an update on the gamepads
-        public int validatedCharacters = 0;   //When this variable is == to connectedControllers -> we can go to game (all players have selected a character)
 
-        //public static PlayerManager singleton = null;
-        public PlayerInput[] playerList;
         void Awake() {
-            // //Check if instance already exists
-            // if (singleton == null) {
-            //     //if not, set instance to this
-            //     singleton = this;
-            // }
-
-            // //If instance already exists and it's not this:
-            // else if (singleton != this) {
-            //     //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-            //     Destroy(gameObject);
-            // }
             DontDestroyOnLoad(this.gameObject);
         }
 
-        // Use this for initialization
         void Start() {
-            connectedControllers = CheckControllerAmount();
-            // Assign_X_Input_Controllers();
-        }
-
-        // void Assign_X_Input_Controllers() {
-        //     for (int i = 0; i < 4; ++i) {
-        //         PlayerIndex controllerID = (PlayerIndex)i;
-        //         GamePadState testState = GamePad.GetState(controllerID);
-        //         if (testState.IsConnected) {
-        //             switch (i) {
-        //                 case 0:
-        //                     controllerID1 = controllerID;
-        //                     controller1state = testState;
-        //                     break;
-        //                 case 1:
-        //                     controllerID2 = controllerID;
-        //                     controller2state = testState;
-        //                     break;
-        //                 case 2:
-        //                     controllerID3 = controllerID;
-        //                     controller3state = testState;
-        //                     break;
-        //                 case 3:
-        //                     controllerID4 = controllerID;
-        //                     controller4state = testState;
-        //                     break;
-        //                 default:
-        //                     break;
-        //             }
-
-        //             Debug.Log(string.Format("GamePad found {0}", controllerID));
-        //         }
-        //     }
-        // }
-
-        //Checks if the amount of controllers changed when connecting/unplugging new controllers
-        int CheckControllerAmount() {
-            int amount = 0;
-
-            for (int i = 0; i < 4; ++i) {
-                PlayerIndex controllerID = (PlayerIndex)i;
-                GamePadState testState = GamePad.GetState(controllerID);
-                if (testState.IsConnected) {
-                    amount++;
-                }
+            //Fill PlayerListDyn
+            for (int i = 1; i < K_NB_PLAYER_MAX+1; ++i) {
+                PlayerData playerData = new PlayerData();
+                playerData.controllerID = i;
+                playerData.Spawn(PlayerPrefab);
+                playerListDyn.Add(playerData);
             }
-
-            return amount;
         }
-
-        // Update is called once per frame
+        
         void Update() {
-            if (use_X_Input) {
-                if (connectedControllers != CheckControllerAmount()) {
-                    connectedControllers = CheckControllerAmount();
-                    print("update controllers");
-                    //Assign_X_Input_Controllers();
-                }
-
-                UpdatePlayersValidated();
-
-                // if (controller1state.IsConnected) {
-                //     controller1state = GamePad.GetState(controllerID1);
-                //     if (controller1state.Buttons.Start == ButtonState.Pressed) {
-                //         print("Controller with ID 0 pressed start");
-                //         //you can call a function here to instantiate a player and then assign this ID to the player input's script to connect the player to the controller that pressed start for example
-                //         //you then also need to assign that player input script to one of the X input modules to connect it with unity's input system
-                //     }
-                // }
-
-                // if (controller2state.IsConnected) {
-                //     controller2state = GamePad.GetState(controllerID2);
-                //     if (controller2state.Buttons.Start == ButtonState.Pressed) {
-                //         print("Controller with ID 1 pressed start");
-                //     }
-                // }
-
-                // if (controller3state.IsConnected) {
-                //     controller3state = GamePad.GetState(controllerID3);
-                //     if (controller3state.Buttons.Start == ButtonState.Pressed) {
-                //         print("Controller with ID 2 pressed start");
-                //     }
-                // }
-
-                // if (controller4state.IsConnected) {
-                //     controller4state = GamePad.GetState(controllerID4);
-                //     if (controller4state.Buttons.Start == ButtonState.Pressed) {
-                //         print("Controller with ID 3 pressed start");
-                //     }
-                // }
-            }
-            // else {
-            //     //join game
-            //     if (Input.GetButtonDown("Start1")) {
-            //         print("start1");
-            //     }
-            //     if (Input.GetButtonDown("Start2")) {
-            //         print("Start2");
-            //     }
-            //     if (Input.GetButtonDown("Start3")) {
-            //         print("Start3");
-            //     }
-            //     if (Input.GetButtonDown("Start4")) {
-            //         print("Start4");
-            //     }
-            // }
-        }
-        void UpdatePlayersValidated() {
-            validatedCharacters = 0;
-            foreach (PlayerInput player in playerList)
-            {
-                if(player.HasSelectedCharacted)
-                {
-                    validatedCharacters++;
-                }
-            }
-
-            if(validatedCharacters == connectedControllers)
-            {
-                print("All players are readyyyy !!"); //Do send player selection to game level Scene scene = SceneManager.GetActiveScene();
-                LoadLevel();
-            }
-        }
-        void LoadLevel() {
             Scene scene = SceneManager.GetActiveScene();
-            if (scene.name != "Level0")
+            if (scene.name == "CharacterSelection")
             {
-                SceneManager.LoadScene("Level0");
+                UpdateCharacterSelectionInputsHandling();
             }
+            
+        }
+
+        void UpdateCharacterSelectionInputsHandling()
+        {
+            for (int i = 1; i < K_NB_PLAYER_MAX+1; ++i) {
+                PlayerData player = FindPlayer(i);
+                if(player != null)
+                {
+                    if(!player.isActive)
+                    {
+                        if(Input.GetButtonDown(K_JUMP+i.ToString())) //First validation / Character Selection
+                        {
+                            player.Activate();
+                        }
+                        else if(Input.GetAxis(K_HORIZONTAL_DPAD+i.ToString()) == 1)
+                        {
+                            player.SelectNextCharacter();
+                        }
+                        else if(Input.GetAxis(K_HORIZONTAL_DPAD+i.ToString()) == -1)
+                        {
+                            player.SelectPreviousCharacter();
+                        }
+                        else if(Input.GetAxis(K_HORIZONTAL_DPAD+i.ToString()) != -1 && Input.GetAxis(K_HORIZONTAL_DPAD+i.ToString()) != 1)
+                        {
+                            player.ReleaseDPad();
+                        }
+                    }
+                    else if(player.isActive)
+                    {
+                        if(Input.GetButtonDown(K_JUMP+i.ToString()))  //Second validation / Ready to play
+                        {
+                            player.Ready();
+                        }
+                        else if(Input.GetButtonDown(K_DASH+i.ToString()))
+                        {
+                            player.Deactivate();
+                        }
+                    }
+                }
+            }
+            
+            UpdatePlayersValidated();
+        }
+
+        void UpdatePlayersValidated() {
+            if(GetControllerAmount() == GetPlayerReadyCount())
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                if (scene.name == "CharacterSelection")
+                {
+                    print("All players are readyyyy !!"); //Do send player selection to game level Scene scene = SceneManager.GetActiveScene();
+                    LoadLevel("Level0");
+                    EnablePlayersGameplayControls();
+                }
+            }
+        }
+        void LoadLevel(string sceneName) {
+            SceneManager.LoadScene(sceneName);
+        }
+        void EnablePlayersGameplayControls()
+        {
+            foreach (PlayerData player in playerListDyn)
+            {
+                if(player.isActive)
+                {
+                    player.ToggleGameplayControls(true);
+                }
+            }
+        }
+        int GetControllerAmount() {
+            return Input.GetJoystickNames().Length;
+        }
+
+        int GetPlayerActiveCount()
+        {
+            int activeCount = 0;
+            foreach (PlayerData player in playerListDyn)
+            {
+                if(player.isActive)
+                {
+                    activeCount++;
+                }
+            }
+            return activeCount;
+        }
+        int GetPlayerReadyCount()
+        {
+            int readyCount = 0;
+            foreach (PlayerData player in playerListDyn)
+            {
+                if(player.isReady)
+                {
+                    readyCount++;
+                }
+            }
+            return readyCount;
+        }
+        bool IsActivePlayerID(int playerIDToCheck) {
+            foreach (PlayerData player in playerListDyn)
+            {
+                if(player.controllerID == playerIDToCheck)
+                {
+                    return player.isActive;
+                }
+            }
+            return false;
+        }
+        PlayerData FindPlayer(int playerControllerID)
+        {
+            foreach (PlayerData player in playerListDyn)
+            {
+                if(player.controllerID == playerControllerID)
+                {
+                    return player;
+                }
+            }
+            return null;
         }
     }
 }
