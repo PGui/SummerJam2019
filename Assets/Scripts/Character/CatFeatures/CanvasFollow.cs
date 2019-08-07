dac204bcca271270088cdae72fe920d93b7f1a8a
Ludo <ludovic.geraert@gmail.com>
2019-08-07 17:48:12 +0200

    Text on its way

--- Assets/Scripts/Character/CatFeatures/CanvasFollow.cs
+++ Assets/Scripts/Character/CatFeatures/CanvasFollow.cs
@@ -6,27 +6,35 @@ using UnityEngine;
 public class CanvasFollow : MonoBehaviour
 {
 
-    public Text playerName;
-    public Text countdown;
-
-    public GameObject player;
-
+    private Text playerName;
+    private Text countdown;
+    private Text score;
+    public GameObject header;
+    private GameObject player;
+    private CatEnergy catEnergy;
     private Camera gameCamera;
+    
   
       // Start is called before the first frame update
     void Start()
     {
         gameCamera = Camera.main;
-        
+        player = this.transform.parent.gameObject;
+
+        catEnergy = player.GetComponent<CatEnergy>();
+        //playerName = header.transform.Find("Name").GetComponent<Text>();
+       //countdown = header.transform.Find("Countdown").GetComponent<Text>();
+         //score = header.transform.Find("Score").GetComponent<Text>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 uiPosition = gameCamera.WorldToScreenPoint(player.transform.position);
-        playerName.transform.position = uiPosition;
+        header.transform.position = uiPosition;
 
-        countdown.text = player.GetComponent<CatEnergy>().energyCountdown.ToString();
-        countdown.transform.position = uiPosition;
+        if(catEnergy != null)
+            countdown.text = catEnergy.energyCountdown.ToString();
     }
 }
