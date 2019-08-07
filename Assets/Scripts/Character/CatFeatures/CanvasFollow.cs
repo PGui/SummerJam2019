4503bc13fd00976f892fffae26f6eac7b30ec4e5
Ludo <ludovic.geraert@gmail.com>
2019-08-07 17:19:05 +0200

    Adding UI elements structure

---
+++ Assets/Scripts/Character/CatFeatures/CanvasFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CanvasFollow : MonoBehaviour
+{
+
+    public Text playerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
+        playerName.transform.position = namePose;
+    }
+}
