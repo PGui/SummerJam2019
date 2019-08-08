842c5284f95f270e016e4966f8820326d6054692
Bastien <babas-91@hotmail.fr>
2019-08-08 15:28:09 +0200

    BlackCat_SwarmRotation

---
+++ Assets/Scripts/Character/CatFeatures/ForceZeroWorldRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceZeroWorldRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(90.0f,-transform.parent.transform.parent.rotation.eulerAngles.y,0);
+    }
+}
